d0c2d0924e5d802a1f341a94361edcded2841c34
shalomTalker <shalom604@gmail.com>
2019-09-12 21:43:45 +0300

    adding verify account feature via sendgrid

--- authServer/app.js
+++ authServer/app.js
@@ -5,26 +5,26 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const debug = require('debug')('chatapp:app');
 const cors = require("cors");
-const mongoose = require('mongoose');
-require('./models/User');
-require('./models/Token');
+// const mongoose = require('mongoose');
+// require('./models/User');
+// require('./models/Token');
 const { mongoURI, cookieKey } = require('./config/keys')
 
 const authRouter = require('./routes/authRouter');
 
 const app = express();
-mongoose.Promise = global.Promise;
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true});
-debug('DB is connected');
+// mongoose.Promise = global.Promise;
+// mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true});
+// debug('DB is connected');
 console.log(process.env);
 
-app.use(
-  cors({
+// app.use(
+//   cors({
     
-    origin: "http://localhost:3000",
-    credentials: true
-  })
-);
+//     origin: "http://localhost:3000",
+//     credentials: true
+//   })
+// );
 app.use(cookieParser());
 app.use(logger('dev'));
 app.use(express.json());
